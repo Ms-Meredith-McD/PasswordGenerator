23d595b2ac6b97a2faf11be523a1f04d68e7c0f5
Ms-Meredith-McD <meredith.e.mcdonald@gmail.com>
2023-12-22 21:45:34 -0600

    fixed character arrays, took out quotes and commas and made them strings instead, was having a problems with getting commas in the passwords

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,10 +18,10 @@ generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
   // Create the list of characters that will be used in the password
-  var lowerCaseArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',];
-  var upperCaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
-  var numbersArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,];
-  var specialArray = ["!", "@", "#", "$", "%", "^", "&", "*",];
+  var lowerCaseArray = 'abcdefghijklmnopqrstuvwxyz';
+  var upperCaseArray = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var numbersArray = '0123456789';
+  var specialArray = '!@#$%&*';
   var userChoice = "";
 
   // Asks the user to pick a password length and then converts it to a number
@@ -65,6 +65,7 @@ function generatePassword() {
 var passwordCompletion = ''
 for (var i = 0; i < passLength; i++) {
   var randomNumber = Math.floor(Math.random() * userChoice.length);
+  console.log(randomNumber)
   passwordCompletion += userChoice[randomNumber];
 }
 return passwordCompletion
